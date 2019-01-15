95d5a8ffc9a0eb11292efbede90049c24ca00748
ParneetP <46293246+ParneetP@users.noreply.github.com>
2019-01-15 14:49:01 +1000

    Update functions.php
    add changes to theme support

--- functions.php
+++ functions.php
@@ -52,7 +52,7 @@ if ( ! function_exists( 'riders_setup' ) ) :
 		 * to output valid HTML5.
 		 */
 		add_theme_support( 'html5', array(
-			
+			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
