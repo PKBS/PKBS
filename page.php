67083c519dfe1ff3ac700809e8773175929ce69c
ParneetP <46293246+ParneetP@users.noreply.github.com>
2019-01-15 15:06:19 +1000

    Update page.php
    change the loop

--- page.php
+++ page.php
@@ -19,7 +19,7 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
+		 ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
