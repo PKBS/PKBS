e857d449abe572c81b005466aeac6841102debe0
Sharnjit <46259942+Sharnjit@users.noreply.github.com>
2019-01-15 17:27:49 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -74,8 +74,8 @@ if ( ! function_exists( 'riders_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'      => 25,
-			'width'       => 25,
+			'height'      => 250,
+			'width'       => 250,
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
